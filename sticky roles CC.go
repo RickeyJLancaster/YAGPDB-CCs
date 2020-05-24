c174d97acc7db168f54aeef26f6f82d31447e70e
Rick:) <mb870497@gmail.com>
2020-05-24 13:34:35 -0700

    Update sticky roles CC.go

--- sticky roles CC.go
+++ sticky roles CC.go
@@ -1,32 +1,33 @@
-
-
-
-// CC 1:
+// CC:
 //  Trigger: Regex `.*`
-## TODO: ADD COOLDOWN
-{{$roles := .Member.Roles}}
-{{$data := (dbGet 69420 "stickyroles").Value}}
-{{$data_converted := sdict $data}}
+{{if not (dbGet .User.ID "cooldown_stickyroles")}}
+    {{dbSetExpire .User.ID "cooldown_stickyroles" "cooldown" 7200}}
+    {{$roles := .Member.Roles}}
+    {{$data := (dbGet 69420 "stickyroles").Value}}
 
-{{if ($data_converted.Get (toString .User.ID))}}
-    {{$retrieved_roles:= ($data_converted.Get (toString .User.ID))}}
-    {{$isequal := false}}
-    {{if eq (len $roles) (len $retrieved_roles)}}
-        {{range $inx, $role := $roles}}{{if eq (index $retrieved_roles $inx) $role}}{{$isequal = true}}{{end}}{{end}}
-        {{if not $isequal}}
-            {{dbSet 69420 "stickyroles" (sdict (toString .User.ID) $roles)}}
+    {{if $data}}
+        {{$data_converted := (sdict $data)}}
+        {{if ($data_converted.Get (toString .User.ID))}}
+            {{$retrieved_roles:= ($data_converted.Get (toString .User.ID))}}
+            {{$isdifferent := true}}
+            {{if eq (len $roles) (len $retrieved_roles)}}
+                {{range $inx, $role := $roles}}{{if eq (index $retrieved_roles $inx) $role}}{{$isdifferent = false}}{{end}}{{end}}
+                {{if $isdifferent}}
+                    {{dbSet 69420 "stickyroles" ($data_converted.Set (toString .User.ID) $roles)}}
+                {{end}}
+            {{else}}
+                {{dbSet 69420 "stickyroles" ($data_converted.Set (toString .User.ID) $roles)}}
+            {{end}}
+        {{else if not ($data_converted.Get (toString .User.ID))}}
+            {{dbSet 69420 "stickyroles" ($data_converted.Set (toString .User.ID) $roles)}}
         {{end}}
-    {{else}}
+    {{else if not $data}}
         {{dbSet 69420 "stickyroles" (sdict (toString .User.ID) $roles)}}
     {{end}}
-{{else if not ($data_converted.Get (toString .User.ID))}}
-    {{dbSet 69420 "stickyroles" (sdict (toString .User.ID) $roles)}}
 {{end}}
-
 
 // Kick DM and Ban DM
 {{dbSetExpire .User.ID "ban_kick" 1 86400}}
-
 
 // Leave Message:
 {{$ban_kick := (dbGet .User.ID "ban_kick")}}
@@ -38,17 +39,12 @@
     {{end}}
 {{end}}
 
-
 // Join Message:
 {{$data := dbGet 69420 "stickyroles"}}
 {{$data_converted := sdict $data}}
-
 {{if ($data_converted.Get .User.ID)}}
     {{$roles := ($data_converted.Get .User.ID)}}
     {{range $roles}}
         {{addRoleID .}}
     {{end}}
 {{end}}
-
-
-
