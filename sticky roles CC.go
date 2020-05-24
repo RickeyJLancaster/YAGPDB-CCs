87324eba688b526a52eeed3039dd9cac65bed7db
Rick:) <mb870497@gmail.com>
2020-05-24 11:44:37 -0700

    Update and rename sticky roles CC to sticky roles CC.go

--- sticky roles CC.go
+++ sticky roles CC.go
@@ -1,6 +1,9 @@
+
+
+
 // CC 1:
 //  Trigger: Regex `.*`
-
+## TODO: ADD COOLDOWN
 {{$roles := .Member.Roles}}
 {{$data := (dbGet 69420 "stickyroles").Value}}
 {{$data_converted := sdict $data}}
@@ -19,3 +22,33 @@
 {{else if not ($data_converted.Get (toString .User.ID))}}
     {{dbSet 69420 "stickyroles" (sdict (toString .User.ID) $roles)}}
 {{end}}
+
+
+// Kick DM and Ban DM
+{{dbSetExpire .User.ID "ban_kick" 1 86400}}
+
+
+// Leave Message:
+{{$ban_kick := (dbGet .User.ID "ban_kick")}}
+{{if $ban_kick}}
+    {{$data := (dbGet 69420 "stickyroles").Value}}
+    {{$data_converted := sdict $data}}
+    {{if ($data_converted.Get (toString .User.ID))}}
+        {{dbSet 69420 "stickyroles" ($data_converted.Del (toString .User.ID))}}
+    {{end}}
+{{end}}
+
+
+// Join Message:
+{{$data := dbGet 69420 "stickyroles"}}
+{{$data_converted := sdict $data}}
+
+{{if ($data_converted.Get .User.ID)}}
+    {{$roles := ($data_converted.Get .User.ID)}}
+    {{range $roles}}
+        {{addRoleID .}}
+    {{end}}
+{{end}}
+
+
+
