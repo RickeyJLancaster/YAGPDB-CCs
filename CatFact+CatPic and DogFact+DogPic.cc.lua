6e962f2621fd29f5836a86ce770c964fce6f2390
Rick:) <mb870497@gmail.com>
2020-06-13 16:06:42 -0700

    Update CatFact+CatPic and DogFact+DogPic.cc.lua

--- CatFact+CatPic and DogFact+DogPic.cc.lua
+++ CatFact+CatPic and DogFact+DogPic.cc.lua
@@ -1,31 +1,18 @@
 {{/*
 DogFact+DogImage and CatFact+CatImage Commands
 To avoid duplication of the response disable the original CatFact and DogFact commands on Core > Command Settings > Command Override
-
-Trigger Type: Regex
-Trigger: ^-(dog|doggo|dogfact|cat|kitten|catfact)
+> Trigger Type: Regex
+> Trigger: ^-(dog|doggo|dogfact|cat|kitten|catfact)
 */}}
-
+{{$stuff:=""}}
+{{$link:=""}}
 {{if (reFind "(dog|doggo|dogfact)" .Cmd)}}
-    
-    {{$title := "Dog Fact:"}}
-    {{$fact := (exec "dogfact")}}
-    {{$x := randInt 1000}}
-    {{$link := joinStr "" "https://placedog.net/500/280/id="  (urlescape (toString $x))}}
-    {{$footer_img:= (joinStr "" "https://cdn.discordapp.com/avatars/" (toString .User.ID) "/" .User.Avatar ".png")}}
-    {{$footer_txt := (joinStr " " "Doggo requested by" .User.Username)}}
-    {{sendMessage nil (cembed "title" $title "description" $fact "image" (sdict "url" $link) "footer" (sdict "text" $footer_txt "icon_url" $footer_img))}}
-
+    {{$stuff = cslice "Doggo" "Dog Fact:" "dogfact"}}
+    {{$link = joinStr "" "https://placedog.net/500/280/id="  (urlescape (toString (randInt 1000)))}}
 {{else if (reFind "(cat|kitten|catfact)" .Cmd)}}
-    
-    {{$title := "Cat Fact:"}}
-    {{$fact := (exec "catfact")}}
-    {{$h := randInt 300 500}}
-    {{$w := randInt 200 600}}
-    {{$x := randInt 0 16}}
-    {{$link := (joinStr "" "http://placekitten.com/" $h "/" $w "?image=" (urlescape (toString $x)))}}
-    {{$footer_img:= (joinStr "" "https://cdn.discordapp.com/avatars/" (toString .User.ID) "/" .User.Avatar ".png")}}
-    {{$footer_txt := (joinStr " " "Kitten requested by" .User.Username)}}
-    {{sendMessage nil (cembed "title" $title "description" $fact "image" (sdict "url" $link) "footer" (sdict "text" $footer_txt "icon_url" $footer_img))}}
-
+    {{$stuff = cslice "Kitten" "Cat Fact:" "catfact"}}
+    {{$link = (print "http://placekitten.com/" (randInt 300 500) "/" (randInt 200 600) "?image=" (urlescape (toString (randInt 0 16))))}}
+{{end}}
+{{with $stuff}}
+    {{sendMessage nil (cembed "title" (index . 1) "description" (exec (index . 2)) "image" (sdict "url" $link) "footer" (sdict "text" (print (index . 0) " requested by " $.User.Username) "icon_url" ($.User.AvatarURL "256")) "color" (randInt 111111 999999))}}
 {{end}}
