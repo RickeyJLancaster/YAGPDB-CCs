365270a7dea7022180776bd6aa30a01d9219e189
Rick:) <mb870497@gmail.com>
2020-06-13 18:08:28 -0700

    Update join-whois-CC.go

--- join-whois-CC.go
+++ join-whois-CC.go
@@ -1,19 +1,55 @@
-{{define "whois"}}
-    {{$roles := .Member.Roles}}{{$pos := 0}}{{$final := 0}}{{range .Guild.Roles}}{{if in $roles .ID}}{{if and (lt $pos .Position) (.Color)}}{{$pos = .Position}}{{$final = .Color}}{{end}}{{end}}{{end}}
-    {{$var_roles := ""}}{{$counter := 0}}{{range $roles}}{{$counter = add 1 $counter}}{{$var_roles = (print $var_roles "<@&" . "> ")}}{{end}}
-    {{$avatar_check := .User.Avatar}}{{$userAvatar := "https://i.imgur.com/joMi99X.png"}}{{if $avatar_check}}{{$userAvatar = (print "https://cdn.discordapp.com/avatars/" .User.ID "/" (toString .User.Avatar) ".png")}}{{end}}
-    {{$un := ""}}{{$un_bool := false}}{{$un_counter := 0}}{{range pastUsernames .User.ID 0}}{{$un_counter = add 1 $un_counter}}{{if (le $un_counter 8)}}{{$un = (print $un "\n" (.Time.Format "Jan,06") ": " .Name)}}{{$un_bool = true}}{{end}}{{end}}
-    {{$nn := ""}}{{$nn_bool := false}}{{$nn_counter := 0}}{{range pastNicknames .User.ID 0}}{{$nn_counter = add 1 $nn_counter}}{{if (le $nn_counter 8)}}{{$nn = (print $nn "\n" (.Time.Format "Jan,06") ": " .Name)}}{{$nn_bool = true}}{{end}}{{end}}
-    {{$fields := cslice (sdict "name" "Account Age:" "value" currentUserAgeHuman "inline" true)}}
-    {{if $un_bool}}{{$fields = $fields.Append (sdict "name" "Last 8 Usernames:" "value" (print "```" $un "```") "inline" false)}}{{end}}
-    {{if $nn_bool}}{{$fields = $fields.Append (sdict "name" "Last 8 Nicknames:" "value" (print "```" $nn "```") "inline" false)}}{{end}}
-    {{$embed := sdict
-        "author" (sdict "name" "USER JOINED")
-        "title" (print "User: " .User.String "\nID: " .User.ID)
-        "description" (print "\n**Sticky Roles Added:** " $var_roles)
-        "thumbnail" (sdict "url" $userAvatar)
-        "color" $final
-        "timestamp" currentTime
-        "fields" $fields}}
-    {{sendMessage nil (cembed $embed)}}{{end}}
-{{sendTemplate nil "whois"}}
+{{$Final:=0}}
+{{$Pos:=0}}
+{{range .Guild.Roles}}
+    {{- if in $.Member.Roles .ID -}}
+        {{- if and (lt $Pos .Position) (.Color) -}}
+            {{- $Pos = .Position -}}
+            {{- $Final = .Color -}}
+        {{- end -}}
+    {{- end -}}
+{{end}}
+{{$Roles := ""}}
+{{$Counter := 0}}
+{{range .Member.Roles}}
+    {{- $Counter = add 1 $Counter -}}
+    {{- $Roles = (print $Roles "<@&" . "> ") -}}
+{{end}}
+{{if $Roles}}
+    {{$Roles = print "\n**Roles:** " $Roles}}
+{{end}}
+{{$fields := cslice (sdict "name" "Account Age:" "value" currentUserAgeHuman "inline" true)}}
+{{$Un :=""}}
+{{$User := pastUsernames .User.ID 0}}
+{{if gt (len $User) 8}}
+    {{$User = slice $User 0 8}}
+{{end}}
+{{range $User}}
+    {{- $Un = (print $Un "\n" (.Time.Format "Jan,06") ": " .Name) -}}
+{{end}}
+{{if $Un}}
+    {{$fields = $fields.Append (sdict "name" "Last 8 Usernames:" "value" (print "```" $Un "```") "inline" false)}}
+{{end}}
+{{$Nn :=""}}
+{{$Nick := pastNicknames .User.ID 0}}
+{{if gt (len $Nick) 8}}
+    {{$Nick = slice $Nick 0 8}}
+{{end}}
+{{range $Nick}}
+    {{- $Nn = (print $Nn "\n" (.Time.Format "Jan,06") ": " .Name) -}}
+{{end}}
+{{if $Nn}}
+    {{$fields = $fields.Append (sdict "name" "Last 8 Nicknames:" "value" (print "```" $Nn "```") "inline" false)}}
+{{end}}
+{{$Avatar:="https://i.imgur.com/joMi99X.png"}}
+{{if .User.Avatar}}
+    {{$Avatar = .User.AvatarURL "256"}}
+{{end}}
+{{$embed := sdict
+    "author" (sdict "name" "USER JOINED")
+    "title" (print "User: " .User.String "\nID: " .User.ID)
+    "description" $Roles
+    "thumbnail" (sdict "url" $Avatar)
+    "color" $Final
+    "timestamp" currentTime
+    "fields" $fields}}
+{{sendMessage nil (cembed $embed)}}
